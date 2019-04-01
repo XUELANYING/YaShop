9aeb5372a5e3afcf268dc55d4a01e8088db582bb
XUELANYING <18735122505@163.com>
2019-04-01 16:22:56 +0800

    项目搭建

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <Foot/>
+  </div>
+</template>
+<script>
+    import Foot from './common/tabbar/index'
+
+    import Search from './views/search'
+
+    export default {
+        components: {
+            Foot,
+            Search
+        }
+    }
+</script>
+<style>
+  #app, .container {
+    height: 100%;
+    width: 100%;
+  }
+
+  .container {
+    padding-top: 1.88rem;
+    padding-bottom: 0.88rem;
+  }
+</style>
